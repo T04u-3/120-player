835319e8b94a8d49bbb006acdeccbc9564b8e6f8
T04u-3 <otoufusann89@gmail.com>
2026-03-07 22:57:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ reverbGain.connect(audioCtx.destination)
 filter.type = "lowpass"
 filter.frequency.value = 2000
 
-let oscillators = {}
+let currentOsc = null
+let currentNote = null
+let breath = 0
 
 function noteToFreq(note){
 return 440 * Math.pow(2,(note-69)/12)
 }
 
 function noteName(n){
-
 const names=["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 return names[n%12]+Math.floor(n/12-1)
-
 }
 
 async function connectMIDI(){
@@ -29,9 +29,7 @@ async function connectMIDI(){
 let midi = await navigator.requestMIDIAccess()
 
 for(let input of midi.inputs.values()){
-
 input.onmidimessage = handleMIDI
-
 }
 
 }
@@ -39,64 +37,82 @@ input.onmidimessage = handleMIDI
 function handleMIDI(msg){
 
 let [cmd,note,vel] = msg.data
-
 let type = cmd & 0xf0
 
+// NOTE ON
 if(type===144 && vel>0){
 
-noteOn(note,vel)
-
-}else if(type===128 || (type===144 && vel===0)){
-
-noteOff(note)
+startNote(note)
 
 }
 
+// NOTE OFF
+if(type===128 || (type===144 && vel===0)){
+
+stopNote()
+
+}
+
+// CC
 if(type===176){
 
 let cc = note
 let val = vel
 
+// BREATH CONTROL
 if(cc===2){
 
-let v = val/127
-gain.gain.value = v
+breath = val/127
 
-document.getElementById("level").style.width=(v*100)+"%"
+gain.gain.value = breath
 
+document.getElementById("level").style.width=(breath*100)+"%"
+
+// 息が止まったら音止める
+if(breath < 0.02){
+stopNote()
 }
 
 }
 
 }
 
-function noteOn(note,vel){
+}
+
+function startNote(note){
+
+// 前の音止める
+stopNote()
+
+if(breath <= 0.02) return
 
 let osc = audioCtx.createOscillator()
 
-let freq = noteToFreq(note)
-
 osc.type="sawtooth"
-
-osc.frequency.value=freq
+osc.frequency.value = noteToFreq(note)
 
 osc.connect(gain)
 
 osc.start()
 
-oscillators[note]=osc
+currentOsc = osc
+currentNote = note
 
 document.getElementById("note").textContent = noteName(note)
 
 }
 
-function noteOff(note){
+function stopNote(){
 
-if(oscillators[note]){
+if(currentOsc){
 
-oscillators[note].stop()
+currentOsc.stop()
+currentOsc.disconnect()
 
-delete oscillators[note]
+currentOsc = null
+currentNote = null
+
+document.getElementById("note").textContent="---"
 
 }
 
